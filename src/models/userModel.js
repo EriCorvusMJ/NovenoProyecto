db33ec140259766652186b3e252e6d8ebfae2f63
EriCorvusMJ <guadalupe.mejia@corvusdata.mx>
2026-05-20 22:17:16 -0600

    Actualización de nuevas tareas al modelo

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,15 @@
-
 class TaskModel {
   constructor() {
-    this.tasks = ['Aprender Git', 'Configurar arquitectura MVC'];
+    this.tasks = [
+      'Aprender Git',
+      'Configurar arquitectura MVC',
+      'Crear rutas en Express',
+      'Conectar base de datos MongoDB',
+      'Implementar autenticación de usuarios',
+      'Diseñar vistas con EJS',
+      'Crear API REST para tareas',
+      'Desplegar proyecto en Render'
+    ];
   }
 
   getTasks() {
